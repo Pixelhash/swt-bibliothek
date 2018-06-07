27edcebb0ecb7db997c1ef529a61b878aba9c68e
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2018-06-07 16:00:38 +0200

    Ready for implementing email-service(#7)

--- src/main/java/de/fhl/swtlibrary/mvc/ReminderController.java
+++ src/main/java/de/fhl/swtlibrary/mvc/ReminderController.java
@@ -1,6 +1,7 @@
 package de.fhl.swtlibrary.mvc;
 
 import com.google.inject.Inject;
+import de.fhl.swtlibrary.model.Author;
 import de.fhl.swtlibrary.model.Book;
 import de.fhl.swtlibrary.model.BookCopy;
 import io.requery.EntityStore;
@@ -35,26 +36,38 @@ public class ReminderController {
   // Initialize the cronjob to fire at 12am every day
   @Scheduled("5s")
   public void doWork() {
-    System.out.println(remind());
+    remind();
   }
 
   public List<BookCopy> remind() {
+    String name;
+    String mail;
+    String title;
+    String author;
+    String message;
+    String subject;
 
-    LocalDateTime date = LocalDateTime.now();
-    Timestamp df = new Timestamp(System.currentTimeMillis());
     List<BookCopy> borrowedBooks = bookCopyEntityStore.select(BookCopy.class)
-      .where(BookCopy.BORROWER_ID.notNull())// && ((Timestamp)BookCopy.RETURN_ON - df) = 3)
+      .where(BookCopy.BORROWER_ID.notNull())
       .get()
       .toList();
 
-    List<BookCopy> borrowedBooksCopy = new ArrayList<>(borrowedBooks);
-
     for (BookCopy b : borrowedBooks){
-      System.out.println(b.getBook().getTitle());
+      if(b.getDaysLeft() == 3){
+        name = b.getBorrower().getFullName();
+        mail = b.getBorrower().getEmail();
+        title = b.getBook().getTitle();
+        author = b.getBook().getAuthors().get(0).toString().trim();
+        StringBuilder fAuthor = new StringBuilder().append(author, 10, author.length()-1);
+
+        message = "Sehr geeehrte(r) " + name + ",\nDies ist eine automatische Erinnerung für Sie, da sie das Buch \"" + title + "\" von " + fAuthor + " in drei Tagen zurückgeben müssen.\n\nMit freundlichen Grüßen\nIhr Bibliotheksteam";
+        subject = "Erinnerung an fällige Buchrückgabe";
+
+      }
     }
 
 
-    return borrowedBooksCopy;
+    return borrowedBooks;
   }
 
 
