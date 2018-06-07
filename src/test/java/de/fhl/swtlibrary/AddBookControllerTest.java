0b103b40eadf5d64a5fc0af792d64582bc680ecf
Friedrich Hudinjan <friedrich.hudinjan@stud.fh-luebeck.de>
2018-06-07 15:50:19 +0200

    Added first Tests (#4)

---
+++ src/test/java/de/fhl/swtlibrary/AddBookControllerTest.java
@@ -0,0 +1,82 @@
+package de.fhl.swtlibrary;
+
+import com.gargoylesoftware.htmlunit.html.*;
+import de.fhl.swtlibrary.model.Book;
+import de.fhl.swtlibrary.model.Category;
+import de.fhl.swtlibrary.util.Paths;
+import io.requery.EntityStore;
+import io.requery.Persistable;
+import org.jooby.test.JoobyRule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AddBookControllerTest {
+  @ClassRule
+  public static JoobyRule app = new JoobyRule(new App());
+  @BeforeClass
+  public static void setup() throws Exception{
+    TestUtil.setup();
+  }
+
+  @Test
+  public void testAddBookButtonFromDashboard() throws Exception {
+    final HtmlPage dashboardPage = TestUtil.loginWithTestUser(1, "Test1234");
+    final HtmlAnchor anchor = dashboardPage.getAnchorByHref(Paths.BOOK_ADD);
+
+    final HtmlPage bookAddPage = anchor.click();
+
+    assertEquals(Paths.BOOK_ADD, bookAddPage.getBaseURL().getPath());
+  }
+
+  @Test
+  public void testAddBookSuccessful() throws Exception {
+    final HtmlPage dashboardPage = TestUtil.loginWithTestUser(1, "Test1234");
+
+    final HtmlAnchor anchor = dashboardPage.getAnchorByHref(Paths.BOOK_ADD);
+
+    final HtmlPage bookAddPage = anchor.click();
+
+    final HtmlForm bookAddForm = bookAddPage.getFormByName("book_add");
+
+    // Get all required fields
+    final HtmlButton bookAddButton = bookAddForm.getButtonByName("book_add_btn");
+    final HtmlInput titleField = bookAddForm.getInputByName("title");
+    final HtmlInput isbnField = bookAddForm.getInputByName("isbn");
+    final HtmlInput releaseYearField = bookAddForm.getInputByName("release_year");
+    final HtmlInput locationField = bookAddForm.getInputByName("location");
+    final HtmlSelect categorySelection = bookAddForm.getSelectByName("category");
+    final HtmlSelect publisherSelection = bookAddForm.getSelectByName("publisher");
+
+    // Setup demo data for assertion
+    String title = "Deutsch für die 5. Klasse";
+    String isbn = "290115228313";
+    String releaseYear = "2016";
+    String location = "R2";
+    String category = "17";
+    String publisher = "4";
+
+    // Type demo data into fields
+    titleField.setValueAttribute(title);
+    isbnField.setValueAttribute(isbn);
+    releaseYearField.setValueAttribute(releaseYear);
+    locationField.setValueAttribute(location);
+    categorySelection.getOptionByValue(category).setSelected(true);
+    publisherSelection.getOptionByValue(publisher).setSelected(true);
+
+    final HtmlPage resultPage = bookAddButton.click();
+
+    EntityStore<Persistable, Book> bookStore = DatabaseUtil.getData();
+
+    Book book = bookStore.select(Book.class)
+      .where(Book.ISBN.equal(isbn))
+      .get().firstOrNull();
+
+    assertNotNull(book);
+  }
+}
