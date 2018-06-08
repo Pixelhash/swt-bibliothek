64528f22cc6b86dd52c5c3c4d1b0c990a5807595
Salpico <bengt.claas.rhodgess@stud.fh-luebeck.de>
2018-06-08 13:47:11 +0200

    Sending emails works(#7)

--- src/main/java/de/fhl/swtlibrary/mvc/ReminderController.java
+++ src/main/java/de/fhl/swtlibrary/mvc/ReminderController.java
@@ -4,30 +4,34 @@ import com.google.inject.Inject;
 import de.fhl.swtlibrary.model.BookCopy;
 import io.requery.EntityStore;
 import io.requery.Persistable;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.jooby.quartz.Scheduled;
 import java.util.List;
 
 public class ReminderController {
 
   private EntityStore<Persistable, BookCopy> bookCopyEntityStore;
+  private SimpleEmail email;
 
   @Inject
-  public ReminderController(
+  public ReminderController(SimpleEmail email,
                             EntityStore<Persistable, BookCopy> bookCopyEntityStore) {
 
     this.bookCopyEntityStore = bookCopyEntityStore;
+    this.email = email;
 
   }
 
   // Initialize the cronjob to fire at 12am every day
-  @Scheduled("5s")
-  public void doWork() {
+  @Scheduled("1m")
+  public void doWork() throws EmailException {
     remind();
   }
 
-  public List<BookCopy> remind() {
+  public List<BookCopy> remind() throws EmailException {
     String name;
-    String mail;
+    String mailAdress;
     String title;
     String author;
     String message;
@@ -41,14 +45,22 @@ public class ReminderController {
     for (BookCopy b : borrowedBooks){
      // if(b.getDaysLeft() == 3){
         name = b.getBorrower().getFullName();
-        mail = b.getBorrower().getEmail();
+        mailAdress = b.getBorrower().getEmail();
         title = b.getBook().getTitle();
         author = b.getBook().getAuthors().get(0).toString().trim();
         StringBuilder fAuthor = new StringBuilder().append(author, 10, author.length()-1);
 
-        message = "Sehr geeehrte(r) " + name + ",\nDies ist eine automatische Erinnerung für Sie, da sie das Buch \"" + title + "\" von " + fAuthor + " in drei Tagen zurückgeben müssen.\n\nMit freundlichen Grüßen\nIhr Bibliotheksteam";
-        subject = "Erinnerung an fällige Buchrückgabe";
-        System.out.println(message);
+        message = "Sehr geeehrte(r) " + name + ",\n\nDies ist eine automatische Erinnerung für Sie, da sie das Buch \"" + title + "\" von " + fAuthor + " in \ndrei Tagen zurückgeben müssen.\n\nMit freundlichen Grüßen\nIhr Bibliotheksteam";
+        subject = "[Bibliothek]Erinnerung an fällige Buchrückgabe";
+        System.out.println("Mail gesendet.");
+
+      {
+        email
+          .setSubject(subject)
+          .setMsg(message)
+          .addTo(mailAdress)
+          .send();
+      }
 
     //  }
     }
