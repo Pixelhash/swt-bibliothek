095bd9ebc111f9758f055b53335436c3a0db8410
Fischer96 <lfischer0612@gmail.com>
2018-06-12 23:09:14 +0200

    Fixed Tests #11

--- src/test/java/de/fhl/swtlibrary/RegisterControllerTest.java
+++ src/test/java/de/fhl/swtlibrary/RegisterControllerTest.java
@@ -1,6 +1,9 @@
 package de.fhl.swtlibrary;
 
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.typesafe.config.Config;
 import de.fhl.swtlibrary.model.User;
 import de.fhl.swtlibrary.util.Paths;
 import io.requery.EntityStore;
@@ -9,6 +12,8 @@ import org.jooby.test.JoobyRule;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -24,24 +29,31 @@ public class RegisterControllerTest {
 
   @Test
   public void testSuccessfulRegistration() throws Exception {
-    final HtmlPage dashboardPage = TestUtil.registerWithTestUser("moin@test.de");
-    assertEquals(Paths.USER_REGISTER, dashboardPage.getBaseURL().getPath());
+    try (SimpleSmtpServer dumbster = SimpleSmtpServer.start(2525)) {
+      final HtmlPage dashboardPage = TestUtil.registerWithTestUser("moin@test.de");
+      assertEquals(Paths.USER_REGISTER, dashboardPage.getBaseURL().getPath());
+    }
   }
 
   @Test
   public void testSuccessfulUser() throws Exception {
-    final HtmlPage dashboardPage = TestUtil.registerWithTestUser("test@test.com");
+    try (SimpleSmtpServer dumbster = SimpleSmtpServer.start(2525)) {
+      final HtmlPage dashboardPage = TestUtil.registerWithTestUser("test@test.com");
 
-    String text = dashboardPage.getBody().querySelector(".is-success").getTextContent().trim();
-    int startIndex = text.indexOf("ID:");
-    String userid = text.substring(startIndex + 4).trim();
+      List<SmtpMessage> emails = dumbster.getReceivedEmails();
+      SmtpMessage mail = emails.get(0);
 
-    EntityStore<Persistable, User> userStore = DatabaseUtil.getData();
-    User user = userStore.select(User.class)
-      .where(User.ID.eq(Integer.parseInt(userid)))
-      .get()
-      .firstOrNull();
+      String text = mail.getBody().trim();
+      int startIndex = text.indexOf("lautet:");
+      String userid = text.substring(startIndex + 8, text.indexOf("Hinweis:")).trim();
 
-    assertTrue(user.getEmail().equals("test@test.com"));
+      EntityStore<Persistable, User> userStore = DatabaseUtil.getData();
+      User user = userStore.select(User.class)
+        .where(User.ID.eq(Integer.parseInt(userid)))
+        .get()
+        .firstOrNull();
+
+      assertTrue(user.getEmail().equals("test@test.com"));
+    }
   }
 }
